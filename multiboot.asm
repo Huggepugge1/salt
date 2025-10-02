fe94fc6c9f919af254f42b88c75726c5a59b9947
Hugo Lindstrom <huggepugge1@gmail.com>
2025-10-02 14:58:47 +0200

    removed old files

--- multiboot.asm
+++
@@ -1,8 +0,0 @@
-; kernel.asm
-BITS 32
-section .multiboot
-align 4
-multiboot_header:
-    dd 0x1BADB002          ; magic number
-    dd 0                    ; flags
-    dd -(0x1BADB002)        ; checksum
